8b8d7eeafc2adbe06502054059c49e4fd26e0075
Nexuslolz <a.klyosov@inbox.ru>
2023-05-21 19:47:29 +0500

    feat: create friend list and adding and removing from friend list

--- client/src/components/Friends/FriendItem/FriendItem.tsx
+++ client/src/components/Friends/FriendItem/FriendItem.tsx
@@ -1,16 +1,74 @@
-import React from 'react';
+import React, { useState } from 'react';
+
+import { useDispatch, useSelector } from 'react-redux';
+import { useNavigate } from 'react-router';
 
 import styles from './FriendItem.module.scss';
 
+import avatar from '../../../assets/person-avatar-placeholder.png';
+import { PeoplePage } from '../../../constants/pages';
+import { getId, getUserFriends } from '../../../store/selectors/userSelector';
+import { userSlice } from '../../../store/slices/userSlice';
 import { IFriendItem } from '../../../types/types';
+import UserService from '../../API/UserService';
+import Button from '../../Button/Button';
 
 const FriendItem: React.FC<IFriendItem> = (props: IFriendItem) => {
+  const friendList = useSelector(getUserFriends);
+  const userId = useSelector(getId);
+
+  const router = useNavigate();
+  const [isFriend, setIsFriend] = useState<boolean>(friendList.includes(props._id) ? true : false);
+  const dispatch = useDispatch();
+
+  const redirectToProduct = () => {
+    window.scrollTo({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+    router(`/friends/${props._id}`);
+  };
+
+  const addFriend = () => {
+    UserService.addFriend(userId, props._id);
+    dispatch(userSlice.actions.addFriend([...friendList, props._id]));
+    setIsFriend(true);
+  };
+
+  const deleteFriend = () => {
+    UserService.deleteFriend(userId, props._id);
+    const deleteFriend = friendList.filter((item) => item !== props._id);
+    dispatch(userSlice.actions.addFriend(deleteFriend));
+    setIsFriend(false);
+  };
+
   return (
-    <div id={props.id} className={styles.friendItem}>
-      <div className={styles.friendItem__imgWrapper}>
-        <img className={styles.friendItem__img} src='' alt='avatar' />
+    <div id={props._id} className={styles.friendItem}>
+      <div onClick={redirectToProduct} className={styles.friendsItem__data}>
+        <div className={styles.friendItem__imgWrapper}>
+          <img className={styles.friendItem__img} src={avatar} alt='avatar' />
+        </div>
+        <div className={styles.friendsItem__content}>
+          <h2 className={styles.friendItem__header}>{props.name}</h2>
+          <p>
+            <span>{PeoplePage.city}</span>
+            {props.body.city}
+          </p>
+          <p>
+            <span>{PeoplePage.register}</span>
+            {new Date(props.createdDate).toLocaleDateString()}
+          </p>
+        </div>
       </div>
-      <h2 className={styles.friendItem__header}>{props.name}</h2>
+      <div className={styles.friendsItem__btns}>
+        <Button additionalClass={styles.friends__btn} text={PeoplePage.chat} />
+        <Button
+          additionalClass={isFriend ? styles.friends__btn_remove : styles.friends__btn}
+          text={isFriend ? PeoplePage.removeFriend : PeoplePage.addFriend}
+          onClick={isFriend ? deleteFriend : addFriend}
+        />
+      </div>
     </div>
   );
 };
--- client/src/components/Friends/FriendList/FriendList.tsx
+++ client/src/components/Friends/FriendList/FriendList.tsx
@@ -1,8 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import { AxiosError } from 'axios';
+import React, { useEffect, useRef, useState } from 'react';
+
+import { useSelector } from 'react-redux';
 
 import styles from './FriendList.module.scss';
 
+import { UserError } from '../../../constants/errors';
+import { API_params, NoPosts } from '../../../constants/pages';
 import useFetch from '../../../hooks/useFetch';
+import { getId } from '../../../store/selectors/userSelector';
 import { IFriendItem } from '../../../types/types';
 import UserService from '../../API/UserService';
 import Loader from '../../Loader/Loader';
@@ -10,20 +16,127 @@ import FriendItem from '../FriendItem/FriendItem';
 
 const FriendList: React.FC = () => {
   const [friends, setFriends] = useState<IFriendItem[]>();
-  const [fetchFriends, isFriendsLoading] = useFetch(async () => {
-    const friendList = await UserService.getAllUsers();
-    setFriends(friendList.data);
+
+  const [skip, setSkip] = useState<number>(0);
+  const [isPaginationLoad, setIsPaginationLoad] = useState<boolean>(false);
+  const [paginationError, setPaginationError] = useState<string>('');
+
+  const myId = useSelector(getId);
+
+  const [fetchFriends, isFriendsLoading, isFriendsError] = useFetch(async () => {
+    const friendList = await UserService.getAllUsers(API_params.limit, skip);
+
+    const pureFriends = friendList.data.filter((user) => {
+      return user._id !== myId;
+    });
+
+    setFriends(pureFriends);
+    setSkip((prev) => (prev += API_params.skip));
   });
 
   useEffect(() => {
     fetchFriends();
+
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
+  const lastElement = useRef<HTMLDivElement | null>(null);
+  const observer = useRef<IntersectionObserver | null>(null);
+
+  const changePage = async (): Promise<void> => {
+    if (friends?.length === 0) return;
+
+    try {
+      const allFriendsLength = (await UserService.getAllUsers()).data.length;
+
+      if (friends?.length! + 1 >= allFriendsLength) {
+        throw new Error('Больше нечего подгружать');
+      }
+
+      setPaginationError('');
+      setIsPaginationLoad(true);
+
+      const response = await UserService.getAllUsers(API_params.limit, skip);
+      const pureFriends = response.data.filter((user) => {
+        return user._id !== myId;
+      });
+      setSkip((prev) => (prev += API_params.skip));
+      setFriends([...friends!, ...pureFriends]);
+    } catch (err: unknown) {
+      if (err instanceof AxiosError) {
+        setPaginationError(err.message);
+      }
+    }
+  };
+
+  useEffect(() => {
+    if (isFriendsLoading || !lastElement.current) return;
+
+    if (observer.current) observer.current.disconnect();
+
+    const callback = function (entries: IntersectionObserverEntry[]) {
+      if (entries[0].isIntersecting) {
+        try {
+          changePage();
+        } catch (err) {
+          console.error(`Error has occured, ${err}`);
+        }
+      }
+    };
+
+    observer.current = new IntersectionObserver(callback);
+    observer.current.observe(lastElement.current);
+    setIsPaginationLoad(false);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [skip]);
+
+  if (isFriendsLoading) {
+    return (
+      <div className={styles.friendsWrapper}>
+        <Loader />
+      </div>
+    );
+  }
+
+  if (isFriendsError) {
+    return (
+      <div className={styles.friendsWrapper}>
+        <h1 className={styles.friendsError}>{`${UserError.postListgetErr}${UserError.postListTryLater}`}</h1>
+      </div>
+    );
+  }
+
   if (!friends) return null;
+
+  if (friends.length === 0) {
+    return (
+      <div className={styles.friendsWrapper}>
+        <h1>{NoPosts.usersFriendPage}</h1>
+      </div>
+    );
+  }
+
   return (
     <div className={styles.friendsWrapper}>
-      {isFriendsLoading ? <Loader /> : friends.map((friend) => <FriendItem name={friend.name} id={friend.id} />)}
+      {friends.map((friend) => (
+        <FriendItem
+          key={friend._id}
+          name={friend.name}
+          body={friend.body}
+          _id={friend._id}
+          createdDate={friend.createdDate}
+        />
+      ))}
+      <div ref={lastElement} className={styles.observer}></div>
+      <div className={styles.paginationParams}>
+        {paginationError ? (
+          <span>
+            {UserError.postListgetErr} {paginationError}. {UserError.userData}
+          </span>
+        ) : (
+          isPaginationLoad && <Loader />
+        )}
+      </div>
     </div>
   );
 };
